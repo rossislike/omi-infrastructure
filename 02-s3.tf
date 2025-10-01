bcddfe7749572ff0113ea801d95bbaf3d03c85cb
rossislike <ross.agginie@gmail.com>
2025-09-30 23:14:41 -0400

    force destroy artifacts bucket

--- 02-s3.tf
+++ 02-s3.tf
@@ -17,6 +17,7 @@ locals {
 resource "aws_s3_bucket" "artifacts" {
   bucket = "${var.project_name}-artifacts-${local.suffix}-${var.environment}"
   tags   = var.tags
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "artifacts" {
