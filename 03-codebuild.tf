1f33f7810b101485cd42e2146789ab2927f7905c
rossislike <ross.agginie@gmail.com>
2025-10-01 00:08:47 -0400

    fix codebuild state access

--- 03-codebuild.tf
+++ 03-codebuild.tf
@@ -50,7 +50,9 @@ resource "aws_iam_role_policy" "codebuild" {
           # "arn:aws:s3:::${aws_s3_bucket.website.bucket}",
           # "arn:aws:s3:::${aws_s3_bucket.website.bucket}/*",
           "arn:aws:s3:::${aws_s3_bucket.artifacts.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.artifacts.bucket}/*"
+          "arn:aws:s3:::${aws_s3_bucket.artifacts.bucket}/*",
+          "arn:aws:s3:::omi-state",
+          "arn:aws:s3:::omi-state/*",
         ]
       },
       {
@@ -81,15 +83,16 @@ resource "aws_codebuild_project" "github" {
   service_role = aws_iam_role.codebuild.arn
 
   artifacts {
-    type = "S3"
-    location = aws_s3_bucket.artifacts.bucket
+    type = "NO_ARTIFACTS"
   }
+
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+
   }
 
   source {
