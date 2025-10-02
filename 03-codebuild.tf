879ec6ac5438df294df44cd096e739805c5cf844
rossislike <ross.agginie@gmail.com>
2025-10-02 18:38:03 -0400

    fix iam

--- 03-codebuild.tf
+++ 03-codebuild.tf
@@ -40,19 +40,15 @@ resource "aws_iam_role_policy" "codebuild" {
       {
         Effect = "Allow"
         Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
+          "s3:*"
         ]
         Resource = [
-          # "arn:aws:s3:::${aws_s3_bucket.website.bucket}",
-          # "arn:aws:s3:::${aws_s3_bucket.website.bucket}/*",
+          "arn:aws:s3:::${aws_s3_bucket.website.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.website.bucket}/*",
           "arn:aws:s3:::${aws_s3_bucket.artifacts.bucket}",
           "arn:aws:s3:::${aws_s3_bucket.artifacts.bucket}/*",
-          "arn:aws:s3:::omi-state",
-          "arn:aws:s3:::omi-state/*",
+          "arn:aws:s3:::${var.state_bucket}",
+          "arn:aws:s3:::${var.state_bucket}/*",
         ]
       },
       {
