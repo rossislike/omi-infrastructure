f3a46c667cc9e43b696d18541cf4ff16550d9b8f
rossislike <ross.agginie@gmail.com>
2025-12-15 09:21:34 -0500

    rem bucket

--- 02-s3.tf
+++ 02-s3.tf
@@ -36,39 +36,6 @@ resource "aws_s3_bucket" "lambda_bucket" {
   tags   = var.tags
 }
 
-resource "aws_s3_bucket" "photos_bucket" {
-  bucket = "${var.project_name}-photos-${var.environment}"
-  tags   = var.tags
-  
-}
-
-resource "aws_s3_bucket_public_access_block" "photos" {
-  bucket = aws_s3_bucket.photos_bucket.id
-  
-  block_public_acls       = false
-  block_public_policy     = false 
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "photos_bucket_policy" {
-  bucket = aws_s3_bucket.photos_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.photos_bucket.arn}/*"
-      }
-    ]
-  })
-  depends_on = [aws_s3_bucket_public_access_block.photos]
-}
-
 resource "aws_s3_bucket_versioning" "artifacts" {
   bucket = aws_s3_bucket.artifacts.id
   versioning_configuration {
